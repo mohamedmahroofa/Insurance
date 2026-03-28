01008bcabe4b1b12e1a224579a4622cce9845e22
mohamedmahroofa <ahm.sheedh@gmail.com>
2026-03-27 21:20:39 -0400

    added the cards to the infoComponent

--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { NgOptimizedImage } from "@angular/common";
+
 
 @Component({
   selector: 'app-info',
   standalone: true,
-  imports: [NgOptimizedImage],
+  imports: [],
   templateUrl: './info.component.html',
   styleUrl: './info.component.css'
 })
