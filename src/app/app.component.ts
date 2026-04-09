efa7b3561241bdd572956ef9f3c0bf23c145be97
mohamedmahroofa <ahm.sheedh@gmail.com>
2026-04-08 23:02:22 -0400

    Added the footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { FooterComponent } from './footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
