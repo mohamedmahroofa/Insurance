2203b556bf76abb2c5770ee41ce31d8a66d14e4a
mohamedmahroofa <ahm.sheedh@gmail.com>
2026-04-24 22:09:10 -0400

    Added routerlink and routerlinkactive to certain dropdown menus

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
 import { HeroCarouselComponent } from '../hero-carousel/hero-carousel.component';
 import { InfoComponent } from '../info/info.component';
+import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NgOptimizedImage, HeroCarouselComponent, InfoComponent],
+  imports: [NgOptimizedImage, HeroCarouselComponent, InfoComponent, RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
